b1e005599e16194578da1b54c5cca5a8ff28095c
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:04:34 +0530

    Add files via upload

---
+++ Prime_Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Prime Number
+        //   The Number which divide by itself only.
+
+        static void Main(string[] args)
+        {
+            int n = 7;
+
+            for (int i = 2; i <=n; i++)
+            {
+                if (n%i==0)
+                {
+                    Console.WriteLine(" This is Prime Number");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+
